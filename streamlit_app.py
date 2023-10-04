c9315dfce1a4118a89f64905590e8e6e9ae420cd
ssivakuma2 <146788674+ssivakuma2@users.noreply.github.com>
2023-10-04 15:59:07 -0500

    Update streamlit_app.py
    Imported Pandas library and added file from amazon s3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('My parents new healthy Diner')
 streamlit.header('Breakfast Favourites')
@@ -8,3 +9,5 @@ streamlit.text('🐔 Hard boiled free range Egg')
 streamlit.text(' 🥑🍞 Avacado toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
